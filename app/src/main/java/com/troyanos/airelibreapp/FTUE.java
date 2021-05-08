d01f3db8ba69d55932d83ee28d6515efa81b834d
taitocode <taitocode@gmail.com>
2021-05-08 13:55:29 -0300

    First Commit with fragments

---
+++ app/src/main/java/com/troyanos/airelibreapp/FTUE.java
@@ -0,0 +1,14 @@
+package com.troyanos.airelibreapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FTUE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_t_u_e);
+    }
+}
